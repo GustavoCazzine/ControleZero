0de383e4325c84b3591e97288e4623c711412b64
Gustavo Cazzine <cazzinengustavo@gmail.com>
2025-08-03 08:56:11 -0300

    feat: Adiciona lógica para registrar uma nova transação

---
+++ core/transacoes.py
@@ -0,0 +1,11 @@
+
+
+def adicionar_transacao(descricao, valor, categoria):
+
+    transacao = {
+        "descricao" : descricao,
+        "valor" : valor,
+        "categoria" : categoria
+    }
+
+    return transacao
